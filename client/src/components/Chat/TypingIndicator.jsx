66181c366fccf1364bc1485ffabab0539c480522
evilbikash67-lab <evilbikash67@gmail.com>
2026-07-13 11:37:24 +0530

    Create TypingIndicator.jsx

---
+++ client/src/components/Chat/TypingIndicator.jsx
@@ -0,0 +1,9 @@
+export default function TypingIndicator() {
+  return (
+    <div className="flex items-center space-x-1 p-4">
+      <div className="typing-dot w-2 h-2 bg-gray-400 rounded-full"></div>
+      <div className="typing-dot w-2 h-2 bg-gray-400 rounded-full"></div>
+      <div className="typing-dot w-2 h-2 bg-gray-400 rounded-full"></div>
+    </div>
+  );
+}
