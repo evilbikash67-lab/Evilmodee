c7cf26424b3288709e51e52139e9bacc8f6c3768
evilbikash67-lab <evilbikash67@gmail.com>
2026-07-13 11:19:59 +0530

    Create modelMap.js

---
+++ server/config/modelMap.js
@@ -0,0 +1,17 @@
+// Alias → Hugging Face model ID (NEVER EXPOSE TO FRONTEND)
+const MODEL_MAP = {
+  "Evil Alpha":   "HauhauCS/Qwen3.6-35B-A3B-Uncensored-HauhauCS-Aggressive",
+  "Evil Beta":    "DavidAU/Qwen3.6-40B-Claude-4.6-Opus-Deckard-Heretic-Uncensored-Thinking-NEO-CODE-Di-IMatrix-MAX-GGUF",
+  "Evil Gamma":   "HauhauCS/Gemma4-12B-QAT-Uncensored-HauhauCS-Balanced",
+  "Evil Delta":   "HauhauCS/Qwen3.5-9B-Uncensored-HauhauCS-Aggressive",
+  "Evil Omega":   "DavidAU/Qwen3.5-9B-Claude-4.6-HighIQ-THINKING-HERETIC-UNCENSORED",
+  "Evil Titan":   "llmfan46/gemma-4-31B-it-uncensored-heretic",
+  "Evil Shadow":  "dphn/Dolphin3.0-Llama3.1-8B",
+  "Evil Phantom": "mlabonne/NeuralDaredevil-8B-abliterated",
+  "Evil Nova":    "DavidAU/L3.2-Rogue-Creative-Instruct-Uncensored-Abliterated-7B-GGUF",
+  "Evil Inferno": "DavidAU/Llama-3.2-8X3B-MOE-Dark-Champion-Instruct-uncensored-abliterated-18.4B-GGUF",
+  "Evil Prime":   "DavidAU/L3.1-Dark-Planet-10.7B-ExxxxxxxxTended-GGUF",
+  "Evil Ultra":   "DavidAU/Llama-3.1-128k-Dark-Planet-Uncensored-8B-GGUF"
+};
+
+module.exports = MODEL_MAP;
